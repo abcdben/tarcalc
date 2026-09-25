c20f3b93a051b3921956d40713fde28d79364fc3
deploy <deploy@tarcalc.com>
2026-09-25 12:22:37 -0500

    deploy 2026-09-25T17:22Z

---
+++ assets/main-C9_h8XtU.js
@@ -0,0 +1,1 @@
+import{a as r,j as t,r as e}from"./styles-Cl5FQBZz.js";import{A as o}from"./App-DE8PNgDk.js";r.createRoot(document.getElementById("root")).render(t.jsx(e.StrictMode,{children:t.jsx(o,{})}));
